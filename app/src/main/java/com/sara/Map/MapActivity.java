430adc7832884dbe7caa25581e5f25563dc42d25
bassem3312 <basem3312@gmail.com>
2016-07-26 15:32:19 +0200

    map

--- app/src/main/java/com/sara/Map/MapActivity.java
+++ app/src/main/java/com/sara/Map/MapActivity.java
@@ -1,13 +1,23 @@
 package com.sara.Map;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.sara.newconcepts.R;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -22,15 +32,86 @@ public class MapActivity extends AppCompatActivity {
 
     private void SetupMap() {
 
-        map = ((MapFragment) getFragmentManager()
-                .findFragmentById(R.id.map)).getMap();
-        LatLng latLng = new LatLng(33.54587, 32.411125);
-        MarkerOptions options = new MarkerOptions();
-        options.title("test");
-        options.snippet("map");
+
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        // map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
+        double latitude = 30.0179962;
+        double longitude = 31.4174193;
+        LatLng latLng = new LatLng(latitude, longitude);
+
+        final MarkerOptions options = new MarkerOptions();
+        // options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker))
+        //       .draggable(true);
+
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLng(latLng);
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(12);
         options.position(latLng);
+        options.draggable(true);
+        options.title("Map Test");
+        options.snippet("New Cairo");
+        options.flat(true);
 
+
+        //  map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12));
         map.addMarker(options);
+        // map.animateCamera(zoom);
 
+
+        map.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+
+                LatLng latLng1 = marker.getPosition();
+
+                marker.setSnippet(latLng1.latitude + " , " + latLng1.longitude);
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+
+                LatLng latLng1 = marker.getPosition();
+
+                //marker.setSnippet(latLng1.latitude + " , " + latLng1.longitude);
+                marker.setSnippet(GetAddressName(latLng1));
+
+            }
+        });
+        CameraPosition cameraPosition = CameraPosition.builder()
+                .target(latLng)
+                .zoom(13)
+                .build();
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18),
+                2000, null);
+    }
+
+    private String GetAddressName(LatLng latLng) {
+
+        String addressName = "";
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            List<Address> lst_address =
+                    geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+
+            if (lst_address.size() > 0) {
+                Address address = lst_address.get(0);
+                if (address != null) {
+                    addressName = address.getCountryName() + " , "
+                            + address.getAdminArea() + ""
+                            + address.getSubAdminArea();
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return addressName;
     }
 }
